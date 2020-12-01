3e853cd521383791f5995e0fd24e85da330de87a
Roman Dubchak <dubrathers@mail.ru>
2020-12-01 23:59:35 +0300

    Create Table File

--- cloud_client/src/main/java/com/geekbrains/cloud/storage/client/gui/ClientGUI.java
+++ cloud_client/src/main/java/com/geekbrains/cloud/storage/client/gui/ClientGUI.java
@@ -1,6 +1,7 @@
 package com.geekbrains.cloud.storage.client.gui;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.HashMap;
 
@@ -10,7 +11,8 @@ public class ClientGUI extends JFrame implements Thread.UncaughtExceptionHandler
     private static final int HEIGHT = 500;
 
     private final JPanel panelTop = new JPanel(new GridLayout(1,3));
-    private final JPanel panelCenter = new JPanel(new GridLayout(4,1));
+//    private final JPanel panelCenter = new JPanel(new GridLayout(2,1));
+    private final Box contentsTable = new Box(BoxLayout.Y_AXIS);
 
 
     private final JTextField jtLogin = new JTextField("login");
@@ -28,14 +30,16 @@ public class ClientGUI extends JFrame implements Thread.UncaughtExceptionHandler
     private final String NAME_TITLE = "Имя файла";
     private final String SIZE_TITLE = "Размер файла";
 
+    private final JLabel localTitle = new JLabel(LOCAL_TITLE);
+
     //        Таблица с файлами
 //    private final HashMap<String, String> fileList = new HashMap<String, String>(); мапа с файлами
-    private final Object[][] tableExample =
+    private Object[][] tableExample = new String[][]
                         {{"text1.txt", "16 KB"},
                         {"text2.txt", "21 KB"},
                         {"text3.txt", "4 KB"}};
 
-    private final Object[] handlerTable = {NAME_TITLE,SIZE_TITLE};
+    private Object[] handlerTable = new String[] {NAME_TITLE,SIZE_TITLE};
 
     private final JTable cloudTable = new JTable(tableExample, handlerTable);
 
@@ -46,15 +50,28 @@ public class ClientGUI extends JFrame implements Thread.UncaughtExceptionHandler
 //        setSize(WIDTH, HEIGHT);
         setTitle(WINDOW_TITLE);
 
-//        cloudLog.setLineWrap(true); // перенос строки в логе по ширине.
-
 
         panelTop.add(jtLogin); // сделать по центру, остальное невидимое.
         panelTop.add(jtPassword);
         panelTop.add(btnLogin); // после успешной регистраиции это панеь невидима
 
-        panelCenter.add(new Label(LOCAL_TITLE), 0);
-        panelCenter.add(cloudTable);
+        // запрет редактироавния ячеек таблицы
+        cloudTable.setModel(new DefaultTableModel(tableExample, handlerTable){
+            @Override
+            public boolean isCellEditable(int row, int column)  {
+                return false;
+            }
+        });
+        cloudTable.setGridColor(Color.LIGHT_GRAY); // цвет сетки
+        contentsTable.add(new JScrollPane(cloudTable));
+
+        /*
+        Добавить
+        -текстовые поля по добавлению файла
+        -Авторизацию + базу клиентов
+        -Действия на кнопки
+        */
+
 
         panelButton.add(btnSendFile, BorderLayout.WEST);
         panelButton.add(btnDeleteFile, BorderLayout.CENTER);
@@ -63,12 +80,10 @@ public class ClientGUI extends JFrame implements Thread.UncaughtExceptionHandler
 //        panelButton.setVisible(false); включить панель с кнопками при входе
 
 
-
-
         add(panelTop, BorderLayout.NORTH); // добавить панель наверх
-        add(panelCenter, BorderLayout.CENTER); // добавить центарльную панель
+        add(contentsTable, BorderLayout.CENTER); // добавить центарльную панель с таблицей
         add(panelButton, BorderLayout.SOUTH);
-//        add(grdCenter, BorderLayout.CENTER);
+
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
